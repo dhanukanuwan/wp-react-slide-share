c24b20c0311a4f3528dd886a75aa9f3e94276061
Dhanuka Nuwan <nuwan.dhanuka@gmail.com>
2019-01-13 23:23:27 +0100

    thumbnail slider click events

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -4,38 +4,70 @@ import './css/main.min.css';
 import Swiper from 'swiper';
 //import $ from 'jquery';
 
-class ImageThumbs extends Component {
+class SingleThumb extends Component {
 
-  constructor( props ) {
-    super( props )
+  constructor() {
+    super()
     this.state = {
-      imageList: []
+      clickedItemSrc: "",
+      clickedItemAlt: ""
     }
   }
 
-  componentDidMount() {
-    fetch('http://meyer.test/wp-json/wp/v2/media')
-      .then(response => response.json())
-      .then(data => this.setState({ imageList: data }));
+  handleClick = (e) => {
 
-    setTimeout(() => { window.dispatchEvent(new Event('resize')) }, 0);
+    const clickedSrc = e.target.getAttribute("original");
+    const clickedAlt = e.target.getAttribute("alt");
+
+    const clickedData = [clickedSrc, clickedAlt];
+
+    this.setState({
+      clickedItemSrc: clickedSrc,
+      clickedItemAlt: clickedAlt
+    })
+
+    this.props.clickedDataHandler( clickedData );
+
   }
 
   render() {
 
-    const imageList = this.state.imageList;
+    const imageData = this.props.imageData;
+
+    return (
+      <div className="swiper-slide">
+        <img src={imageData.media_details.sizes.slide_thumb.source_url} onClick={this.handleClick} alt={imageData.title.rendered} original={imageData.guid.rendered} />
+      </div>
+    );
+
+  }
+
+}
+
+class ImageThumbs extends Component {
+
+  componentDidMount() {
+    setTimeout(() => { window.dispatchEvent(new Event('resize')) }, 0);
+  }
+
+  handleClickedData = (passedData) => {
+    this.props.clickedDataHandler( passedData );
+  }
+
+  render() {
+
+    const imageList = this.props.thumbList;
 
     const thumbSwiper = new Swiper( '.thumbs-wrap', {
         direction: 'vertical',
         slidesPerView: 1,
-        mousewheel: true,
         height: 140
     });
 
     return (
       <div className="thumbs-wrap swiper-container">
         <div className="swiper-wrapper">
-          {imageList.map( ( image , i ) => <div key={i} className="swiper-slide" ><img src={image.media_details.sizes.slide_thumb.source_url} alt={image.title.rendered} /></div>)}
+          {imageList.map( ( image , i ) => <SingleThumb clickedDataHandler={this.handleClickedData} key={i} imageData={image} /> )}
         </div>
       </div>
     );
@@ -44,6 +76,31 @@ class ImageThumbs extends Component {
 }
 
 class App extends Component {
+
+  constructor( props ) {
+    super( props )
+    this.state = {
+      imageList: [],
+      bigImageUrl: "",
+      bigImgageAlt: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://meyer.test/wp-json/wp/v2/media/?per_page=100')
+      .then(response => response.json())
+      .then(data => this.setState({ imageList: data, bigImageUrl: data[0].guid.rendered, bigImgageAlt: data[0].title.rendered }));
+
+    setTimeout(() => { window.dispatchEvent(new Event('resize')) }, 0);
+  }
+
+  handleClickedData = (passedData) => {
+    this.setState({
+      bigImageUrl: passedData[0],
+      bigImgageAlt: passedData[1]
+    });
+  }
+
   render() {
     return (
       <div className="App full-height">
@@ -56,10 +113,19 @@ class App extends Component {
                   <img src={logo} className="img-responsive" alt="app-logo" />
                 </a>
 
-                <ImageThumbs />
+                <ImageThumbs clickedDataHandler={this.handleClickedData} thumbList={this.state.imageList} />
 
               </div>
             </div>
+
+            <div className="col-sm-12 col-md-6">
+              <div className="slide-share-middle full-height">
+
+                <img src={this.state.bigImageUrl} alt={this.state.bigImgageAlt} />
+
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
