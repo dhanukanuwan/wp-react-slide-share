053b9b3f4aaa7949b1c8539c90a8b1170f868963
Dhanuka Nuwan <nuwan.dhanuka@gmail.com>
2019-01-27 01:05:16 +0100

    changes to gitignore

---
+++ react-src/public/index.php
@@ -0,0 +1,41 @@
+<?php
+  $TEMPLATE_PATH = parse_url(get_template_directory_uri(), PHP_URL_PATH);
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <!--
+        manifest.json provides metadata used when your web app is added to the
+        homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/
+    -->
+    <link rel="manifest" href="<?php echo $TEMPLATE_PATH; ?>/manifest.json">
+    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico?" type="image/x-icon">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
+    <body <?php body_class(); ?>>
+    <noscript>
+        You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+    <!--
+        This PHP file is a template.
+        If you open it directly in the browser, you will see an empty page.
+
+        You can add webfonts, meta tags, or analytics to this file.
+        The build step will place the bundled scripts into the <body> tag.
+
+        To begin the development, run `npm run wpstart` or `yarn wpstart`.
+        To create a production bundle, use `npm run wpbuild` or `yarn wpbuild`.
+    -->
+    </body>
+</html>
